8606838aee9cc13dda3095da956be655eaa3db38
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-11-20 12:06:41 -0300

    Add: Tree Terminado.

--- tp-11/usuarioTree.cpp
+++ tp-11/usuarioTree.cpp
@@ -56,6 +56,9 @@ int aparicionesT(int e, Tree t){
 }
 
 int max(int x, int y){
+// PROPÓSITO: Indica si el primer número es mayor al segundo.
+// COSTO OPERACIONAL: O(1).
+// COSTO MEMORIA: O(1).
     return x > y ? x : y;
 }
 
@@ -72,31 +75,76 @@ int heightT(Tree t){
     return altura;
 }
 
+void addToListIn(Tree t, ArrayList xs){
+// PROPÓSITO: Agrega los elementos del árbol dado a la lista dada.
+// COSTO OPERACIONAL: O(N).
+// COSTO MEMORIA: O(N).
+    if(!isEmptyT(t)){
+        addToListIn(left(t), xs);
+        addToListIn(right(t), xs);
+        add(rootT(t), xs);
+    }
+} 
+
 ArrayList toList(Tree t){
 // PROPÓSITO: Dado un árbol devuelve una lista con todos sus elementos.
 // COSTO OPERACIONAL: O(N).
 // COSTO MEMORIA: O(N).
-    int size = sizeT(t);    
-    ArrayList xs = newArrayListWith(size);
-    add(rootT(t), xs);
+    ArrayList xs = newArrayListWith(sizeT(t));
+    addToListIn(t, xs);
+    return xs; 
+}
 
-    
-
-    return 
+void addLeavesTo(Tree t, ArrayList xs){
+// PROPÓSITO: Agrega todas las hojas del árbol dado a la lista dada.
+// COSTO OPERACIONAL: O(N).
+// COSTO MEMORIA: O(N).
+    if(!isEmptyT(t)){
+        if(isEmptyT(left(t)) && isEmptyT(right(t))){
+            add(rootT(t), xs);
+        } else {
+            addLeavesTo(left(t), xs);
+            addLeavesTo(right(t), xs);
+        }
+    }
 }
 
 ArrayList leaves(Tree t){
 // PROPÓSITO: Dado un árbol devuelve los elementos que se encuentran en sus hojas.
 // COSTO OPERACIONAL: O(N).
 // COSTO MEMORIA: O(N).
+    ArrayList xs = newArrayListWith(sizeT(t));
+    addLeavesTo(t, xs);
+    return xs;
+}
 
+void addLevelNTo(Tree t, int n, ArrayList xs){
+// PROPÓSITO: Dados un número n, un árbol devuelve y una lista, agrega los nodos del nivel n en la lista dada.
+// PRECONDICIÓN: El valor dado tiene que ser mayor igual a 0.
+// COSTO OPERACIONAL: O(N).
+// COSTO MEMORIA: O(N). 
+    if(!isEmptyT(t)){
+        if(n == 0){
+            add(rootT(t), xs);
+        } else {
+            addLevelNTo(left(t), n-1, xs);
+            addLevelNTo(right(t), n-1, xs);
+        }
+    }
 }
 
 ArrayList levelN(int n, Tree t){
-// PROPÓSITO: Dados un número n y un árbol devuelve una lista con los no dos de nivel n.
+// PROPÓSITO: Dados un número n y un árbol devuelve una lista con los nodos de nivel n.
+// PRECONDICIÓN: El valor dado tiene que ser mayor igual a 0.
 // COSTO OPERACIONAL: O(N).
 // COSTO MEMORIA: O(N).
+    if(n < 0){
+        throw new runtime_error("El valor dado no es válido.");
+    }
 
+    ArrayList xs = newArrayListWith(sizeT(t));
+    addLevelNTo(t, n, xs);
+    return xs;
 }
 
 /* INTERFAZ DE TREE:
