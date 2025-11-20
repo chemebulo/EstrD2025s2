c653d3d2500d91ae43fc8aa47ccc5373e0dc92fc
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-11-20 01:06:39 -0300

    Add: Queue Terminado.

--- tp-11/Queue.cpp
+++ tp-11/Queue.cpp
@@ -5,56 +5,104 @@ using namespace std;
 Queue emptyQ(){
 // PROPÓSITO: Crea una cola vacía.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
-
+// COSTO MEMORIA: O(1).
+    Queue q = new QueueSt();
+    q->size = 0;
+    q->first = NULL;
+    q->last = NULL;
+    return q;
 }
 
 bool isEmptyQ(Queue q){
 // PROPÓSITO: Indica si la cola está vacía.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
-
+// COSTO MEMORIA: O(1).
+    return q->size == 0;
 }
 
 int firstQ(Queue q){
 // PROPÓSITO: Devuelve el primer elemento.
+// PRECONDICIÓN: La Queue no está vacía.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
-
+// COSTO MEMORIA: O(1).
+    if(q->size == 0){
+        throw new runtime_error("La Queue está vacía.");
+    }
+    return q->first->value;
 }
 
 void Enqueue(int x, Queue q){
 // PROPÓSITO: Agrega un elemento al final de la cola.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
+// COSTO MEMORIA: O(1).
+    NodoQQ nuevo = new NodoQ();
+    nuevo->value = x;
+    nuevo->next = NULL;
 
+    if(q->last == NULL){
+        q->first = nuevo;
+    } else{ 
+        q->last->next = nuevo;
+    }
+    
+    q->last = nuevo;
+    q->size++;
 }
 
 void Dequeue(Queue q){
 // PROPÓSITO: Quita el primer elemento de la cola.
+// PRECONDICIÓN: La Queue no está vacía.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
+// COSTO MEMORIA: O(1).
+    if(q->size == 0){
+        throw new runtime_error("La Queue está vacía.");
+    }
 
+    NodoQQ temp = q->first;
+    
+    if(q->first->next == NULL){
+        q->last = NULL;
+    }
+    
+    q->first = temp->next;
+    delete temp;
+    q->size--;
 }
 
 int lengthQ(Queue q){
 // PROPÓSITO: Devuelve la cantidad de elementos de la cola.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
-
+// COSTO MEMORIA: O(1).
+    return q->size;
 }
 
 void MergeQ(Queue q1, Queue q2){
 // PROPÓSITO: Anexa Q2 al final de Q1, liberando la memoria inservible de Q2 en el proceso.
 // NOTA: Si bien se libera memoria de Q2, no necesariamente la de sus nodos.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
-
+// COSTO MEMORIA: O(1).
+    if(q1->first == NULL){
+        q1->first = q2->first;
+    } else {
+        q1->last->next = q2->first;
+    }
+    
+    q1->last = q2->last;
+    q1->size += q2->size;
+    delete q2;
 }
 
 void DestroyQ(Queue q){
 // PROPÓSITO: Libera la memoria ocupada por la cola.
 // COSTO OPERACIONAL: O(N).
-// COSTO MEMORIA: O().
+// COSTO MEMORIA: O(1).
+    NodoQQ temp;
 
+    while(q->first != NULL){
+        temp = q->first;
+        q->first = q->first->next;
+        delete temp;
+    }
+
+    delete q;
 }
--- tp-11/Queue.h
+++ tp-11/Queue.h
@@ -13,8 +13,8 @@ typedef NodoQ* NodoQQ; // INV. REP.: El puntero no es NULL.
 
 struct QueueSt {
     /* INVARIANTES DE REPRESENTACIÓN:
-        * Size es la cantidad de nodos a recorrer desde first hasta llegar a last.
-        * 
+        * Size es la cantidad de nodos a recorrer desde first hasta llegar a NULL.
+        * Si last != NULL, entonces last->next es NULL, y además first != NULL.
     */
     int size;     // Cantidad de elementos.
     NodoQ* first; // Puntero al primer nodo.
@@ -28,42 +28,44 @@ typedef QueueSt* Queue; // INV. REP.: El puntero no es NULL.
 Queue emptyQ();
 // PROPÓSITO: Crea una cola vacía.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
+// COSTO MEMORIA: O(1).
 
 bool isEmptyQ(Queue q);
 // PROPÓSITO: Indica si la cola está vacía.
+// PRECONDICIÓN: La Queue no está vacía.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
+// COSTO MEMORIA: O(1).
 
 int firstQ(Queue q);
 // PROPÓSITO: Devuelve el primer elemento.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
+// COSTO MEMORIA: O(1).
 
 void Enqueue(int x, Queue q);
 // PROPÓSITO: Agrega un elemento al final de la cola.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
+// COSTO MEMORIA: O(1).
 
 void Dequeue(Queue q);
 // PROPÓSITO: Quita el primer elemento de la cola.
+// PRECONDICIÓN: La Queue no está vacía.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
+// COSTO MEMORIA: O(1).
 
 int lengthQ(Queue q);
 // PROPÓSITO: Devuelve la cantidad de elementos de la cola.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
+// COSTO MEMORIA: O(1).
 
 void MergeQ(Queue q1, Queue q2);
 // PROPÓSITO: Anexa Q2 al final de Q1, liberando la memoria inservible de Q2 en el proceso.
 // NOTA: Si bien se libera memoria de Q2, no necesariamente la de sus nodos.
 // COSTO OPERACIONAL: O(1).
-// COSTO MEMORIA: O().
+// COSTO MEMORIA: O(1).
 
 void DestroyQ(Queue q);
 // PROPÓSITO: Libera la memoria ocupada por la cola.
 // COSTO OPERACIONAL: O(N).
-// COSTO MEMORIA: O().
+// COSTO MEMORIA: O(1).
 
 // ########################################################################################################################## //
