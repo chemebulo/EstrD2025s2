e22caa7b4bf8427a28aba287a28ea1dfe01dfcbb
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-11-23 02:34:21 -0300

    Add: Usuario BinaryHeap.

--- tp-11/usuarioBinaryHeap.cpp
+++ tp-11/usuarioBinaryHeap.cpp
@@ -26,5 +26,22 @@ BinHeap crearHeap(int* elements, int cant){
 */
 
 int main(){
+    BinHeap h = emptyHeap();
+    cout << boolalpha << "La Heap Binaria se encuentra vacia: " << isEmptyHeap(h) << endl;
+    InsertH(50, h);
+    InsertH(4, h);
+    InsertH(7, h);
+    InsertH(21, h);
+    InsertH(18, h);
+    InsertH(99, h);
+    InsertH(2, h);
+    cout << endl;
+    cout << boolalpha << "Despues de haber agregado elementos, la Heap Binaria se encuentra vacia: " << isEmptyHeap(h) << endl;
+    cout << endl;
+    cout << "El elemento minimo en la Heap Binaria es " << findMin(h) << "." << endl;
+    cout << endl;
+    deleteMin(h);
+    deleteMin(h);
+    cout << "Despues de haber borrado dos veces el minimo, el elemento minimo en la Heap Binaria es " << findMin(h) << "." << endl;
     return 0;
 }
