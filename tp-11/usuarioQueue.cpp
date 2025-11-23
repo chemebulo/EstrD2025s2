9532d8b53ebe33ee13ad5abc4a7e5ffae2d33738
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2025-11-23 02:36:05 -0300

    Add: Usuario Queue.

--- tp-11/usuarioQueue.cpp
+++ tp-11/usuarioQueue.cpp
@@ -14,5 +14,36 @@ using namespace std;
 */
 
 int main(){
+    Queue q1 = emptyQ();
+    Queue q2 = emptyQ();
+    cout << "La longitud de la primera cola es " << lengthQ(q1) << "." << endl; 
+    cout << "La longitud de la segunda cola es " << lengthQ(q2) << "." << endl; 
+    Enqueue(5, q1);
+    cout << endl;
+    cout << boolalpha << "La primera cola esta vacia: " << isEmptyQ(q1) << endl; 
+    cout << boolalpha << "La segunda cola esta vacia: " << isEmptyQ(q2) << endl; 
+    Enqueue(15, q1);
+    Enqueue(25, q1);
+    Enqueue(4, q2);
+    Enqueue(3, q2);
+    Enqueue(2, q2);
+    Enqueue(1, q2);
+    cout << endl;
+    cout << "El primer elemento de la primera cola es " << firstQ(q1) << "." << endl;
+    cout << "El primer elemento de la segunda cola es " << firstQ(q2) << "." << endl;
+    cout << endl;
+    cout << "Despues de haber agregado elementos, la longitud de la primera cola es " << lengthQ(q1) << "." << endl; 
+    cout << "Despues de haber agregado elementos, la longitud de la segunda cola es " << lengthQ(q2) << "." << endl; 
+    cout << endl;
+    Dequeue(q1);
+    cout << "Despues de haber quitado un elemento, la longitud de la primera cola es " << lengthQ(q1) << "." << endl; 
+    cout << "El primer elemento de la primera cola es " << firstQ(q1) << "." << endl;
+    cout << endl;
+    MergeQ(q1, q2);
+    cout << "Despues de haber mergeado ambas colas, la longitud de la cola resultante es " << lengthQ(q1) << "." << endl; 
+    cout << "Despues de haber mergeado ambas colas, la longitud de la segunda cola es " << lengthQ(q2) << ", lo cual no tiene sentido porque fue borrada." << endl; 
+    cout << endl;
+    DestroyQ(q1);
+    cout << "La longitud de la primera cola es " << lengthQ(q1) << ", lo cual no tiene sentido porque fue borrada." << endl;
     return 0;
 }
